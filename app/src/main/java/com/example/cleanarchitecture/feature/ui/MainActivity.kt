3265015b79cd15080fc5e116a0a38998d7f2c47b
agusCod3 <agus.romero.salazar@gmail.com>
2024-05-13 16:02:17 -0400

    Se le agrega un boton para que se note que la vista reacciona

--- app/src/main/java/com/example/cleanarchitecture/feature/ui/MainActivity.kt
+++ app/src/main/java/com/example/cleanarchitecture/feature/ui/MainActivity.kt
@@ -2,26 +2,37 @@ package com.example.cleanarchitecture.feature.ui
 
 import android.os.Bundle
 import android.util.Log
-import android.view.SurfaceControl
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import com.example.cleanarchitecture.R
+import com.example.cleanarchitecture.databinding.ActivityMainBinding
 import com.example.cleanarchitecture.feature.data.model.Transaccion
 import com.example.cleanarchitecture.feature.viewmodel.MainViewModel
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var viewModel: MainViewModel
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val button = binding.button
+        setupViewModel()
+        button.setOnClickListener {
+             viewModel.getListTransactions("id1")
+        }
+
+    }
+
+    private fun setupViewModel(){
+
         viewModel = ViewModelProvider(this)[MainViewModel::class.java]
 
         val transactionsObserver = Observer<List<Transaccion>> {
-             Log.e("Transacciones",it.toString())
+            Log.e("Transacciones",it.toString())
         }
 
         viewModel.getLiveDataObserver().observe(this,transactionsObserver)
